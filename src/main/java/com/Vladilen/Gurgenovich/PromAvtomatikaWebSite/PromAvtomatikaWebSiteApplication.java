3d0e69426ded4aafcbcd7ca7124a492c3c3493e2
Vladimir Naumov <mus-t-ang777@mail.ru>
2025-11-18 10:54:49 +0400

    initial project

---
+++ src/main/java/com/Vladilen/Gurgenovich/PromAvtomatikaWebSite/PromAvtomatikaWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.Vladilen.Gurgenovich.PromAvtomatikaWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromAvtomatikaWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromAvtomatikaWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vladilen/Gurgenovich/PromAvtomatikaWebSite/PromAvtomatikaWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vladilen.Gurgenovich.PromAvtomatikaWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromAvtomatikaWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
