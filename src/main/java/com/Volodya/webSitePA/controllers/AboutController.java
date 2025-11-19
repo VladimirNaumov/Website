a32ad76a10b071f375f11a8067a318d1c0b79106
Vladimir Naumov <mus-t-ang777@mail.ru>
2025-11-19 16:46:43 +0400

    home page+about incorrect

--- src/main/java/com/Volodya/webSitePA/controllers/AboutController.java
+++ src/main/java/com/Volodya/webSitePA/controllers/AboutController.java
@@ -1,28 +1,15 @@
 package com.Volodya.webSitePA.controllers;
 
-
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class MainController {
-
-    @GetMapping("/")
-    public String home (Model model) {
-        model.addAttribute("title", "Главная страница");
-        return "home";
-    }
+public class AboutController {
 
     @GetMapping("/about")
-    public String about (Model model) {
+    public String about(Model model) {
         model.addAttribute("title", "О нас");
         return "about";
     }
 }
-
-
-
-
